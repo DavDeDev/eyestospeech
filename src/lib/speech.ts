15fe0cedf8b752acd02f77d1b427765dae2c6981
David Pietrocola <pietrocoladavid@gmail.com>
2024-11-30 14:56:46 -0500

    fix: speech

--- src/lib/speech.ts
+++ src/lib/speech.ts
@@ -1,5 +1,7 @@
 export function speak(text: string) {
+  window.speechSynthesis.cancel();
   if ('speechSynthesis' in window) {
+    console.log('Speaking:', text)
     const utterance = new SpeechSynthesisUtterance(text)
     window.speechSynthesis.speak(utterance)
   } else {
